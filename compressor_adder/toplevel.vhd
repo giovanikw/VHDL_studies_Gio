3f3b5a12797c607a2e302bdbfd6645954ffe1274
giovanikw <giovanikwi@gmail.com>
2026-08-18 10:21:03 -0300

    minor changes in readability

--- compressor_adder/toplevel.vhd
+++ compressor_adder/toplevel.vhd
@@ -3,15 +3,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 --define entity
-entity toplevel is 
+entity toplevel is
 port(
-		--in
+		--inputs
 		A : in std_logic;
 		B : in std_logic;
 		C : in std_logic;
 		D : in std_logic;
 		Cin : in std_logic;
-		--out
+		--outputs
 		Cout : out std_logic;
 		Carry : out std_logic;
 		Sum : out std_logic
@@ -19,31 +19,31 @@ port(
 
 end toplevel;
 
---define behaviour
-	architecture behaviour of toplevel is
-	signal Xor1: std_logic;
-	signal Xor2 : std_logic;
-	signal Xor3 : std_logic;
-	signal Xor4 : std_logic;
-	begin
-		Xor1 <= A xor B;
-		Xor2 <= C xor D;
-		Xor3 <= Xor1 xor Xor2;
-		Xor4 <= Cin xor Xor3;
-		
-		mux1 : entity work.mux
-			port map(
-				A => A,
-				B => C,
-				Sel => Xor1,
-				Q => Cout
-			);
-		mux2 : entity work.mux
-			port map(
-				A => D,
-				B => Cin,
-				Sel => Xor3,
-				Q => Carry
-			);
-		Sum <= Xor4;
+architecture behaviour of toplevel is
+		signal Xor1 : std_logic;
+		signal Xor2 : std_logic;
+		signal Xor3 : std_logic;
+		signal Xor4 : std_logic;
+begin
+-- xor stages
+	Xor1 <= A xor B;
+	Xor2 <= C xor D;
+	Xor3 <= Xor1 xor Xor2;
+	Xor4 <= Cin xor Xor3;
+	
+	mux1 : entity work.mux
+		port map(
+			A => A,
+			B => C,
+			Sel => Xor1,
+			Q => Cout
+		);
+	mux2 : entity work.mux
+		port map(
+			A => D,
+			B => Cin,
+			Sel => Xor3,
+			Q => Carry
+		);
+	Sum <= Xor4;
 end behaviour;
